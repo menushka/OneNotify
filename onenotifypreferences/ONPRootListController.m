08f39177736388bf6da625c76137bd2cebf33012
Menushka Weeratunga <tmweeratunga@gmail.com>
2019-05-16 23:53:45 -0400

    Added preference bundle

---
+++ onenotifypreferences/ONPRootListController.m
@@ -0,0 +1,13 @@
+#include "ONPRootListController.h"
+
+@implementation ONPRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
